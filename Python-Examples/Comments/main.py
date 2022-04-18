0ee76e1320caf38026ee517e252fe0ca16aa6520
KarmicBacklash <austynrbellinger@gmail.com>
2022-04-18 04:01:59 -0700

    Added more indepth explaination

---
+++ Python-Examples/Comments/main.py
@@ -0,0 +1,11 @@
+# Comments can be done in one of a few ways. The first is like this, one-liners are done with just a # symbol
+# Nothing is stopping you from using comments on several lines, but sometimes you don't want to mess with syntax like that.
+
+print ("Hello World") # Comments can also be done at the end of code.
+
+"""
+Alternatively, you can do multi-line comments with 3 Quote marks
+
+Just like this
+
+"""
--- Python-Examples/Hello-World/main.py
+++ Python-Examples/Hello-World/main.py
@@ -1,1 +1,13 @@
-print ("Hello World")
+# print simply prints whatever value you have in the parenthesis.
+
+print ("Hello World") # This is how you print a string
+
+x = "Hello World"
+
+print (x) # This will print a variable
+
+# You can also print non strings
+
+x = 2+4
+
+print (x) # This prints an Integer
