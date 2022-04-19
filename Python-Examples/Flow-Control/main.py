416ff40885a16c18fb316be13ae73dc0de85bced
KarmicBacklash <austynrbellinger@gmail.com>
2022-04-18 21:35:41 -0700

    Added Flow Control

---
+++ Python-Examples/Flow-Control/main.py
@@ -0,0 +1,12 @@
+# Flow control in python works very simply.
+
+if x = 1: #This is an if statement, if the condition after "if" is true, it executes the following command
+    print("hello world") # This is what it does if the condition is true
+
+elif X = 2: # This command will execute if the variable equals something else specifically.
+    print("hello other world")
+
+else: # Finally, this part executes if anything else besides the pre-described
+    print("goodbye cruel world")
+
+#-----#
