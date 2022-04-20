45df945705794fe2eb8bf5df0cf5202c9e70603e
Timothy Marshall <36028284+LunarMusician@users.noreply.github.com>
2022-04-19 20:46:09 -0700

    Update main.py
    Added comment header

--- Pong/main.py
+++ Pong/main.py
@@ -1,3 +1,12 @@
+"""
+Program Name:           Pong
+Program Creators:       KarmicBacklash & LunarMusician
+Program Purpose:        To understand the basics of Pygame and practicing Python by making a clone of Pong
+Date Created:           18 April, 2022
+Date Modified:          19 April, 2022
+Project Notes:          
+"""
+
 # This will be where we start with pygame
 
 import pygame
