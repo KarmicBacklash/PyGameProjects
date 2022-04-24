18d180504fe3e6a0332c2ea2e98e6d142e2007c6
KarmicBacklash <austynrbellinger@gmail.com>
2022-04-24 09:56:50 -0700

    Added Integer Support to my calculator

--- Python-Learning-Projects/Austyn's Projects/1. Beginner/1. Calculator/main.py
+++ Python-Learning-Projects/Austyn's Projects/1. Beginner/1. Calculator/main.py
@@ -1,12 +1,31 @@
 # Build an arithmetic calculator, must account for possible breaking inputs.
 
+from tokenize import Name
+
+
 error = 0
+
+# Old Method
+
+#while True:
+#    try:
+#        inputOne = int(input("Please enter the first number: "))
+#        break
+#    except ValueError:
+#        print("\nthis is not a valid number. Try Again: ")
 
 while True:
     try:
-        inputOne = int(input("Please enter the first number: "))
-        break
-    except ValueError:
+        inputOne = eval(input("Please enter the first number: "))
+        if type(inputOne) == float:
+            break
+        elif type(inputOne == int):
+            break
+        elif type((inputOne) == str):
+            pass
+    except SyntaxError:
+        print("\nthis is not a valid number. Try Again: ")
+    except NameError:
         print("\nthis is not a valid number. Try Again: ")
 
 while True:
@@ -17,11 +36,27 @@ while True:
     else:
         print("Error: Invalid Function")
 
+# Old Method
+
+#while True:
+#    try:
+#        inputTwo = int(input("Please enter the Second number: "))
+#        break
+#    except ValueError:
+#        print("\nthis is not a valid number. Try Again: ")
+
 while True:
     try:
-        inputTwo = int(input("Please enter the Second number: "))
-        break
-    except ValueError:
+        inputTwo = eval(input("Please enter the first number: "))
+        if type(inputTwo) == float:
+            break
+        elif type(inputTwo == int):
+            break
+        elif type((inputOne) == str):
+            pass
+    except SyntaxError:
+        print("\nthis is not a valid number. Try Again: ")
+    except NameError:
         print("\nthis is not a valid number. Try Again: ")
 
 if inputFunc == "*":
